a69dbf7b6b0373d349095ee77af648fd3968cacc
anandaandikabp <ananda.andika18@gmail.com>
2022-07-08 11:11:15 +0700

    task json

--- app.js
+++ app.js
@@ -1,67 +1,12 @@
-const validator = require('validator');
-const readline = require("readline");
-const { resolve } = require('path');
-const fs = require('fs');
+const contact = require('./contact');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// program
+const main = async () => {
+const nama = await contact.question('Nama? ');    
+const mobile = await contact.question('Nomor HP? ');
+const email = await contact.question('Email? ');
 
-// fungsi input with validator
-// const askNama = () => {
-//     return new Promise((resolve) => {
-//         rl.question('Nama? ', (nama) => {
-//             resolve(nama);
-//         });
-//     });
-// };
+contact.answer(nama, mobile, email);
+};
 
-// const askNomor = () => {
-//     return new Promise((resolve) => {
-//         rl.question('Nomor HP? ', (nomor) => {
-//             if (!validator.isMobilePhone(`${nomor}`,'id-ID')) {
-//                 askNama();
-//             } else {
-//                 askEmail();
-//             }
-//             resolve(nomor);
-//         });
-//     });
-// };
-
-// const askEmail = () => {
-//     return new Promise((resolve) => {
-//         rl.question('Email? ', (email) => {
-//             if (!validator.isEmail(`${email}`)) {
-//                 askNama();
-//             } 
-//             resolve(email);
-//         });
-//     });
-// };
-
-// const askUser = async () => {
-//     const nama = await askNama();    
-//     const nomor = await askNomor();
-//     const email = await askEmail();
-
-//     console.log(`Nama diriku ${nama}, bernomor hp ${nomor},  emailnya ${email}`);
-//     rl.close();
-// };
-
-    rl.question('Nama? ', (nama) => {
-        rl.question('mobile? ', (mobile) => {
-            const contact ={nama,mobile};
-            const file = fs.readFileSync('data/contacts.json','utf-8');
-            const contacts = JSON.parse(file);
-            contacts.push(contact);
-            fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
-            // console.log('Thankyou');
-            console.log(`Nama diriku ${nama}, bernomor hp ${mobile}`);
-            rl.close();
-        });
-    });
-
-
-// askUser();
+main();
---
+++ contact.js
@@ -0,0 +1,38 @@
+const readline = require("readline");
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// buat folder jika belum ada
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+// buat file json jika belum ada
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]','utf-8');
+}
+
+const question = (ask) => {
+    return new Promise((resolve,reject) => {
+        rl.question(ask, (inputVariable) => {
+            resolve(inputVariable);
+        });
+    });
+};
+
+const answer = (nama, mobile, email) => {
+const contact = {nama,mobile,email};
+const file = fs.readFileSync('data/contacts.json','utf-8');
+const contacts = JSON.parse(file);
+contacts.push(contact);
+fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
+console.log('Thankyou');
+rl.close();
+};
+
+module.exports = {question, answer};
